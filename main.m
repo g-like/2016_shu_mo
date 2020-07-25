3376a8639a023e3dc28e37089a53aec92a4dfc6c
g-like <64114787+g-like@users.noreply.github.com>
2020-07-25 13:43:55 +0800

    精简代码，虽然不知道为什么要做，但是我忍不住做了

--- main.m
+++ main.m
@@ -30,44 +30,36 @@ for i = 1:4%生成出入口位置坐标，储存在enExitLabel矩阵中
 
 end
 
+enExit = [];
+
+for i = 1:length(enExitLabel(:, 1))%去除处在原点的坐标
+
+    if enExitLabel(i, 1) ~= 0 || enExitLabel(i, 2) ~= 0
+        enExit(end + 1, :) = enExitLabel(i, :);
+    end
+
+end
+
 %假设路况不拥堵时小区开放造成的影响
-count = zeros(4, 1); %定义一个count数组,用来存储自定义随机生成的出入口特征值
-count(1) = enExitLabel(1, 1);
-count(2) = enExitLabel(2, 2);
-count(3) = enExitLabel(3, 1);
-count(4) = enExitLabel(4, 2);
-k = find(count);
-num = sum(sum(count ~= 0)); %统计count中非零元素个数
-
-enExitDistance = [];
-
-if num == 1
+if length(enExit(:, 1)) == 1
     influence = 0; %如果只有一个出入口，开放小区根本没用，对该路段作用为0
 else %排除只有一个出入口的下述计算
 
-    for j = 1:length(k)%元素遍历enExitLabel
+    for j = 1:length(enExit(:, 1))%下标遍历enExitLabel
         gap = 10; %初始化最短距离
 
         for i = 1:length(nodeLabel(:, 1))%下标遍历nodeLabel
 
-            if two_distance(enExitLabel(k(j), :), nodeLabel(i, :)) <= gap%如果距离小于原先的最短距离
-                gap = two_distance(enExitLabel(k(j), :), nodeLabel(i, :));
+            if two_distance(enExit(j, :), nodeLabel(i, :)) <= gap%如果距离小于原先的最短距离
+                gap = two_distance(enExit(j, :), nodeLabel(i, :));
             end
 
         end
 
-        enExitDistance(end + 1) = gap;
+        enExit(j, 3) = gap;
     end
 
     %汇总上述数据到enExit上,第一列代表横坐标，第二列代表纵坐标，第三列代表与之相距最近节点距离
-    enExit = [];
-
-    for i = 1:length(k)
-        enExit(i, 1) = enExitLabel(k(i), 1);
-        enExit(i, 2) = enExitLabel(k(i), 2);
-        enExit(i, 3) = enExitDistance(i);
-    end
-
     figure('Name', '生成小区图'); %画个图看看
     scatter(enExit(:, 1), enExit(:, 2), 'filled')
     hold on
